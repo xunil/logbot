df0e116ab412d9ecae7cee2f9e70cfa3e24c6a1f
Robert Liesenfeld <xunil@squareup.com>
2011-02-01 21:58:34 -0800

    More !future functionality, options is now a global.

--- logbot.rb
+++ logbot.rb
@@ -3,29 +3,31 @@ require 'rubygems'
 require 'yaml'
 require 'isaac'
 
-options = YAML.load(File.open('logbot.yaml'))
+$options = YAML.load(File.open('logbot.yaml'))
+$future_messages = {}
 
-if options.has_key?(:logdir)
-    Dir.chdir(options[:logdir])
+if $options.has_key?(:logdir)
+    Dir.chdir($options[:logdir])
 end
 
 configure do |c|
-    c.nick = options[:nick]
-    c.server = options[:server]
-    c.port = options[:port] 
-    c.ssl = options[:ssl]
-    c.password = options[:password]
+    c.nick = $options[:nick]
+    c.server = $options[:server]
+    c.port = $options[:port] 
+    c.ssl = $options[:ssl]
+    c.password = $options[:password]
+    c.verbose = $options.has_key?(:verbose) ? $options[:verbose] : false
 end
 
 # Helpers
 helpers do
     def is_admin?(nick)
-        if not options.has_key?[:admins] or options[:admins].empty?
+        if not $options.has_key?(:admins) or $options[:admins].empty?
             # Everyone is an admin!
             return true
         end
 
-        options[:admins].each do |admin_nick|
+        $options[:admins].each do |admin_nick|
             if admin_nick == nick
                 return true
             end
@@ -33,29 +35,64 @@ helpers do
 
         return false
     end
+
+    def help_message(nick)
+        msg nick, "logbot understands !quit, !join #channel, !part, !future, and !help."
+        msg nick, "!part will leave the channel in which the command is heard."
+        msg nick, "!future nickname message will leave a message for nickname,"
+        msg nick, "to be delivered the next time they join the channel."
+        msg nick, "only admins can use !quit, !join, and !part."
+    end
+
+    def store_future(from, to, message)
+        if not $future_messages.has_key?(to)
+            $future_messages[to] = []
+        end
+        $future_messages[to] << {:from => from, :message => message}
+    end
+
+    def has_future?(nick)
+        return $future_messages.has_key?(nick)
+    end
+
+    def get_future(nick)
+        if $future_messages.has_key?(nick)
+            return $future_messages[nick]
+        else
+            return []
+        end
+    end
 end
 
 on :connect do
-    if not options.has_key?(:channels) or options[:channels].empty?
+    if not $options.has_key?(:channels) or $options[:channels].empty?
         join '#logbot'
     else
-        options[:channels].each {|c| join c}
+        $options[:channels].each {|c| join c}
     end
 end
 
 # Commands
-on :channel, /^\!(help|quit|part|join)([ \t]+[^ ]+)*/ do
+on :channel, /^\!(help|quit|part|join|future)([ \t]+.*)*$/ do
     if is_admin?(nick)
-        case matches[0]
-            when "help"
-                help_message(nick)
+        case match[0]
             when "quit"
                 quit "Requested to quit"
             when "part"
                 part channel
             when "join"
-                join matches[1]
+                join match[1]
         end
+    end
+
+    case match[0]
+        when "help"
+            help_message(nick)
+        when "future"
+            to_nick = match[1].split(' ')[0]
+            future_message = match[1].split(' ')[1..-1]
+            store_future(nick, to_nick, future_message)
+            msg channel, "#{nick}: i'll tell #{to_nick} when i see them."
     end
 end
 
@@ -71,4 +108,16 @@ on :channel, /^[^!]/ do
     logfile_name = "%d/%02d/%d-%02d-%02d.%s.log" % [now.year, now.month, now.year, now.month, now.day, channel]
     timestamp = "%02d:%02d:%02d" % [now.hour, now.min, now.sec]
     File.open(logfile_name, "a") {|f| f.puts "#{timestamp} <#{nick}> #{message}"}
+end
+
+on :join do
+    if nick != $options[:nick]
+        msg channel, "yo #{nick}"
+        if has_future?(nick)
+            msg channel, "#{nick}: got a message for you"
+            get_future(nick).each do |future|
+                msg channel, "#{future[:from]} said to tell you #{future[:message]}"
+            end
+        end
+    end
 end
